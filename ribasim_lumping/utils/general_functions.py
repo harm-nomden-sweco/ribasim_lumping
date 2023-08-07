4d095ceb01a386905e3241e294ce9d0854f109a7
tessa-andringa-sweco <tessa.andringa@sweco.nl>
2023-08-07 16:22:57 +0200

    no message

--- ribasim_lumping/utils/general_functions.py
+++ ribasim_lumping/utils/general_functions.py
@@ -1,9 +1,14 @@
+import numpy as np
+import pandas as pd
 import geopandas as gpd
 from shapely.ops import nearest_points
+from typing import Dict, List
 
 
 def find_nearest_nodes(
-    search_locations: gpd.GeoDataFrame, nodes: gpd.GeoDataFrame, id_column: str
+    search_locations: gpd.GeoDataFrame, 
+    nodes: gpd.GeoDataFrame, 
+    id_column: str
 ) -> gpd.GeoDataFrame:
     nearest_node_ids = []
     for index, row in search_locations.iterrows():
@@ -12,4 +17,92 @@ def find_nearest_nodes(
         _, nearest_geom = nearest_points(point, multipoint)
         nearest_node = nodes.loc[nodes["geometry"] == nearest_geom]
         nearest_node_ids.append(nearest_node[id_column].iloc[0])
-    return nearest_node_ids
+    projected_points = gpd.GeoDataFrame(
+        data={id_column: nearest_node_ids},
+        geometry=search_locations['geometry'],
+        crs=search_locations.crs
+    )
+    return projected_points
+
+
+def find_nearest_edges(
+    search_locations: gpd.GeoDataFrame, 
+    edges: gpd.GeoDataFrame, 
+    id_column: str, 
+    selection: str = None,
+    tolerance: int = 100,
+    crs: int = 28992
+) -> gpd.GeoDataFrame:
+    """Function to find nearest linestring including nearest location on edge"""
+    bbox = search_locations.bounds + [-tolerance, -tolerance, tolerance, tolerance]
+    hits = bbox.apply(lambda row: list(edges.sindex.intersection(row)), axis=1)
+    tmp = pd.DataFrame({
+        "split_node_i": np.repeat(hits.index, hits.apply(len)),
+        "mesh1d_nEdges": np.concatenate(hits.values)
+    })
+    if selection is not None and selection in search_locations and selection in edges:
+        tmp = tmp.merge(
+            search_locations.reset_index()[selection], 
+            how='outer', 
+            left_on='split_node_i', 
+            right_index=True
+        ).rename(columns={selection: f'{selection}_x'})
+
+    tmp = tmp.merge(edges, how='inner', left_on='mesh1d_nEdges', right_on='mesh1d_nEdges')
+    tmp = tmp.join(search_locations.geometry.rename("point"), on="split_node_i")
+    tmp = gpd.GeoDataFrame(tmp, geometry="geometry", crs=search_locations.crs)
+    
+    tmp["snap_dist"] = tmp.geometry.distance(gpd.GeoSeries(tmp.point))
+    tmp = tmp.loc[tmp.snap_dist <= tolerance]
+    tmp = tmp.sort_values(by=["snap_dist"])
+
+    if selection is not None and selection in search_locations and selection in edges:
+        tmp = tmp[tmp[selection] == tmp[f'{selection}_x']].copy()
+        tmp = tmp.drop(columns=[f'{selection}_x'])
+
+    tmp_points = tmp.groupby("split_node_i").first()
+    tmp_points['projection'] = tmp_points.apply(
+        lambda x: nearest_points(x.geometry, x.point)[0], 
+        axis=1
+    )
+    tmp_points['projection_x'] = tmp_points['projection'].apply(lambda x: x.x)
+    tmp_points['projection_y'] = tmp_points['projection'].apply(lambda x: x.y)
+    tmp_points = (tmp_points[[id_column, 'projection_x', 'projection_y', 'point']]
+                  .rename(columns={'point': 'geometry'})
+                  .reset_index(drop=True))
+
+    projected_points = gpd.GeoDataFrame(tmp_points, geometry="geometry", crs=crs)
+    return projected_points
+
+
+def create_objects_gdf(
+    data: Dict,
+    xcoor: List[float],
+    ycoor: List[float],
+    edges_gdf: gpd.GeoDataFrame,
+    selection: str = None,
+    crs: int = 28992,
+    tolerance: int = 100,
+):
+    gdf = gpd.GeoDataFrame(
+        data=data, 
+        geometry=gpd.points_from_xy(xcoor, ycoor), 
+        crs=crs
+    )
+    projected_points = find_nearest_edges(
+        search_locations=gdf, 
+        edges=edges_gdf, 
+        id_column='mesh1d_nEdges',
+        selection=selection,
+        tolerance=tolerance,
+        crs=crs
+    )
+    gdf = gpd.GeoDataFrame(
+        data=(gdf.drop(columns='geometry')
+              .merge(projected_points, how='outer', left_index=True, right_index=True)),
+        geometry='geometry',
+        crs=crs
+    )
+    return gdf
+
+
--- ribasim_lumping/utils/get_dhydro_network_objects.py
+++ ribasim_lumping/utils/get_dhydro_network_objects.py
@@ -2,55 +2,51 @@ import geopandas as gpd
 import numpy as np
 import pandas as pd
 from typing import Dict, List
+from .general_functions import create_objects_gdf
+import hydrolib.core.dflowfm as hcdfm
 
 
-def create_objects_gdf(
-    data: Dict,
-    xcoor: List[float],
-    ycoor: List[float],
-    nodes_gdf: gpd.GeoDataFrame,
-    crs: int = 28992,
-):
-    gdf = gpd.GeoDataFrame(
-        data=data, geometry=gpd.points_from_xy(xcoor, ycoor), crs=crs
-    )
-    return gdf.sjoin(nodes_gdf).drop(columns=["index_right"])
-
-
-def get_dhydro_network_objects(map_data, his_data, crs):
+def get_dhydro_network_objects(map_data, his_data, crs, file_bc):
     """Extracts nodes, edges, confluences, bifurcations, weirs, pumps, laterals from his/map"""
     if map_data is None:
         raise ValueError("D-Hydro simulation map-data is not read")
     if his_data is None:
         raise ValueError("D-Hydro simulation his-data is not read")
-    nodes_gdf = get_nodes_dhydro_network(map_data, crs)
-    edges_gdf = get_edges_dhydro_network(map_data, crs)
-    weirs_gdf = get_weirs_dhydro_network(his_data, nodes_gdf, crs)
-    pumps_gdf = get_pumps_dhydro_network(his_data, nodes_gdf, crs)
-    laterals_gdf = get_laterals_dhydro_network(his_data, nodes_gdf, crs)
-    confluences_gdf = get_confluences_dhydro_network(nodes_gdf, edges_gdf)
-    bifurcations_gdf = get_bifurcations_dhydro_network(nodes_gdf, edges_gdf)
-    print(
-        "dhydro-network analysed: nodes/edges/confluences/bifurcations/weirs/pumps/laterals"
-    )
-    return nodes_gdf, edges_gdf, weirs_gdf, pumps_gdf, laterals_gdf, \
-        confluences_gdf, bifurcations_gdf
+    print("D-HYDRO-network analysed:")
+    nodes, nodes_h = get_nodes_dhydro_network(map_data, crs)
+    edges, edges_q = get_edges_dhydro_network(map_data, crs)
+    stations = get_stations_dhydro_network(his_data, edges, crs)
+    pumps = get_pumps_dhydro_network(his_data, edges, crs)
+    weirs = get_weirs_dhydro_network(his_data, edges, crs)
+    orifices = get_orifices_dhydro_network(his_data, edges, crs)
+    bridges = get_bridges_dhydro_network(his_data, edges, crs)
+    culverts = get_culverts_dhydro_network(his_data, edges, crs)
+    uniweirs = get_uniweirs_dhydro_network(his_data, edges, crs)
+    confluences = get_confluences_dhydro_network(nodes, edges)
+    bifurcations = get_bifurcations_dhydro_network(nodes, edges)
+    boundaries = get_boundaries(file_bc, nodes)
+    return nodes, nodes_h, edges, edges_q, stations, pumps, weirs, \
+        orifices, bridges, culverts, uniweirs, confluences, bifurcations, boundaries
 
 
 def get_nodes_dhydro_network(map_data, crs) -> gpd.GeoDataFrame:
     """calculate nodes dataframe"""
+    print(" - nodes and waterlevels")
     nodes_gdf = (
         map_data["mesh1d_node_id"]
         .ugrid.to_geodataframe()
         .reset_index()
         .set_crs(crs)
-    )
+    ).drop(columns=['mesh1d_node_x', 'mesh1d_node_y'])
     nodes_gdf["mesh1d_node_id"] = nodes_gdf["mesh1d_node_id"].astype(str).apply(lambda r: r[2:-1].strip())
-    return nodes_gdf
+    nodes_h_df = map_data.mesh1d_s1.to_dataframe()[['mesh1d_s1']]
+    nodes_h_df = nodes_h_df.reorder_levels(['mesh1d_nNodes', 'set', 'condition'])
+    return nodes_gdf, nodes_h_df
 
 
 def get_edges_dhydro_network(map_data, crs) -> gpd.GeoDataFrame:
     """calculate edges dataframe"""
+    print(" - edges and discharges")
     edges = (
         map_data["mesh1d_q1"][-1][-1]
         .ugrid.to_geodataframe()
@@ -66,58 +62,141 @@ def get_edges_dhydro_network(map_data, crs) -> gpd.GeoDataFrame:
     edges_gdf = edges.merge(
         edges_nodes, how="inner", left_index=True, right_index=True
     )
-    return edges_gdf
+    edges_q_df = map_data.mesh1d_q1.to_dataframe()[['mesh1d_q1']]
+    edges_q_df = edges_q_df.reorder_levels(['mesh1d_nEdges', 'set', 'condition'])
+    return edges_gdf, edges_q_df
+
+
+def get_stations_dhydro_network(his_data, edges_gdf, crs) -> gpd.GeoDataFrame:
+    """Get stations from dhydro_model"""
+    print(" - stations", end="", flush=True)
+    stations_gdf = create_objects_gdf(
+        data={"mesh1d_node_id": his_data.stations},
+        xcoor=his_data.station_geom_node_coordx,
+        ycoor=his_data.station_geom_node_coordy,
+        edges_gdf=edges_gdf[["mesh1d_nEdges", "geometry"]],
+        crs=crs,
+    )
+    stations_gdf['object_type'] = 'station'
+    return stations_gdf
+
+
+def get_pumps_dhydro_network(his_data, edges_gdf, crs) -> gpd.GeoDataFrame:
+    """Get pumps from dhydro_model"""
+    print(" / pumps", end="", flush=True)
+    pumps_gdf = create_objects_gdf(
+        data={"mesh1d_node_id": his_data.pumps},
+        xcoor=his_data.pump_input_geom_node_coordx,
+        ycoor=his_data.pump_input_geom_node_coordy,
+        edges_gdf=edges_gdf[["mesh1d_nEdges", "geometry"]],
+        crs=crs,
+    )
+    pumps_gdf['object_type'] = 'pump'
+    return pumps_gdf
+
+
+def get_weirs_dhydro_network(his_data, edges_gdf, crs) -> gpd.GeoDataFrame:
+    """Get weirs from dhydro_model"""
+    print(" / weirs", end="", flush=True)
+    weirs_gdf = create_objects_gdf(
+        data={"mesh1d_node_id": his_data.weirgens},
+        xcoor=his_data.weir_input_geom_node_coordx,
+        ycoor=his_data.weir_input_geom_node_coordy,
+        edges_gdf=edges_gdf[["mesh1d_nEdges", "geometry"]],
+        crs=crs,
+    )
+    weirs_gdf['object_type'] = 'weir'
+    return weirs_gdf
+
+
+def get_orifices_dhydro_network(his_data, edges_gdf, crs) -> gpd.GeoDataFrame:
+    """Get orifices from dhydro_model"""
+    print(" / orifices", end="", flush=True)
+    orifices_gdf = create_objects_gdf(
+        data={"mesh1d_node_id": his_data.orifice},
+        xcoor=his_data.orifice_input_geom_node_coordx,
+        ycoor=his_data.orifice_input_geom_node_coordy,
+        edges_gdf=edges_gdf[["mesh1d_nEdges", "geometry"]],
+        crs=crs,
+    )
+    orifices_gdf['object_type'] = 'orifice'
+    return orifices_gdf
+
+
+def get_bridges_dhydro_network(his_data, edges_gdf, crs) -> gpd.GeoDataFrame:
+    """Get bridges from dhydro_model"""
+    print(" / bridges", end="", flush=True)
+    bridges_gdf = create_objects_gdf(
+        data={"mesh1d_node_id": his_data.bridge},
+        xcoor=his_data.bridge_input_geom_node_coordx,
+        ycoor=his_data.bridge_input_geom_node_coordy,
+        edges_gdf=edges_gdf[["mesh1d_nEdges", "geometry"]],
+        crs=crs,
+    )
+    bridges_gdf['object_type'] = 'bridge'
+    return bridges_gdf
+
+
+def get_culverts_dhydro_network(his_data, edges_gdf, crs) -> gpd.GeoDataFrame:
+    """Get culverts from dhydro_model"""
+    print(" / culverts", end="", flush=True)
+    culverts_gdf = create_objects_gdf(
+        data={"mesh1d_node_id": his_data.culvert},
+        xcoor=his_data.culvert_input_geom_node_coordx,
+        ycoor=his_data.culvert_input_geom_node_coordy,
+        edges_gdf=edges_gdf[["mesh1d_nEdges", "geometry"]],
+        crs=crs,
+    )
+    culverts_gdf['object_type'] = 'culvert'
+    return culverts_gdf
+
+
+def get_uniweirs_dhydro_network(his_data, edges_gdf, crs) -> gpd.GeoDataFrame:
+    """Get weirs from dhydro_model"""
+    print(" / uniweirs", end="", flush=True)
+    uniweirs_gdf = create_objects_gdf(
+        data={"mesh1d_node_id": his_data.universalWeirs},
+        xcoor=his_data.uniweir_input_geom_node_coordx,
+        ycoor=his_data.uniweir_input_geom_node_coordy,
+        edges_gdf=edges_gdf[["mesh1d_nEdges", "geometry"]],
+        crs=crs,
+    )
+    uniweirs_gdf['object_type'] = 'uniweir'
+    return uniweirs_gdf
 
 
 def get_confluences_dhydro_network(nodes_gdf, edges_gdf) -> gpd.GeoDataFrame:
     """calculate confluence points based on finding multiple inflows"""
+    print(" / confluences", end="", flush=True)
     c = edges_gdf.end_node_no.value_counts()
     confluences_gdf = nodes_gdf[
         nodes_gdf.index.isin(c.index[c.gt(1)])
-    ]
+    ].reset_index(drop=True)
+    confluences_gdf.object_type = 'confluence'
     return confluences_gdf
 
 
 def get_bifurcations_dhydro_network(nodes_gdf, edges_gdf) -> gpd.GeoDataFrame:
     """calculate split points based on finding multiple outflows"""
+    print(" / bifurcations", end="", flush=True)
     d = edges_gdf.start_node_no.value_counts()
     bifurcations_gdf = nodes_gdf[
         nodes_gdf.index.isin(d.index[d.gt(1)])
-    ]
+    ].reset_index(drop=True)
+    bifurcations_gdf.object_type = 'bifurcation'
     return bifurcations_gdf
 
 
-def get_weirs_dhydro_network(his_data, nodes_gdf, crs) -> gpd.GeoDataFrame:
-    """Get weirs from dhydro_model"""
-    weirs_gdf = create_objects_gdf(
-        data={"weirgen": his_data["weirgens"]},
-        xcoor=his_data["weirgen_geom_node_coordx"].data[::2],
-        ycoor=his_data["weirgen_geom_node_coordy"].data[::2],
-        nodes_gdf=nodes_gdf[["mesh1d_nNodes", "geometry"]],
-        crs=crs,
-    )
-    return weirs_gdf
+def get_boundaries(file_bc, nodes):
+    print(" / boundaries")
+    forcingmodel_object = hcdfm.ForcingModel(file_bc)
+    bc = pd.DataFrame([forcing.dict() for forcing in forcingmodel_object.forcing])
 
+    # convert dictionary with boundary type to columns
+    bc = pd.concat([bc.drop(['quantityunitpair'], axis=1), pd.DataFrame.from_records(bc['quantityunitpair'])[0].apply(pd.Series)], axis=1)
 
-def get_pumps_dhydro_network(his_data, nodes_gdf, crs) -> gpd.GeoDataFrame:
-    """Get pumps from dhydro_model"""
-    pumps_gdf = create_objects_gdf(
-        data={"pumps": his_data["pumps"]},
-        xcoor=his_data["pump_geom_node_coordx"].data[::2],
-        ycoor=his_data["pump_geom_node_coordy"].data[::2],
-        nodes_gdf=nodes_gdf[["mesh1d_nNodes", "geometry"]],
-        crs=crs,
-    )
-    return pumps_gdf
-
-
-def get_laterals_dhydro_network(his_data, nodes_gdf, crs) -> gpd.GeoDataFrame:
-    """Get laterals from dhydro_model"""
-    laterals_gdf = create_objects_gdf(
-        data={"lateral": his_data["lateral"]},
-        xcoor=his_data["lateral_geom_node_coordx"],
-        ycoor=his_data["lateral_geom_node_coordy"],
-        nodes_gdf=nodes_gdf[["mesh1d_nNodes", "geometry"]],
-        crs=crs,
-    )
-    return laterals_gdf
+    # merge boundary with nodes
+    boundaries_gdf = nodes.merge(bc,left_on = 'mesh1d_node_id', right_on = 'name')
+    boundaries_gdf = boundaries_gdf.drop(columns=['offset','factor','vertpositionindex','name', 'comments','datablock'])
+    boundaries_gdf.insert(0, 'boundary_id', range(len(boundaries_gdf)))
+    return boundaries_gdf
