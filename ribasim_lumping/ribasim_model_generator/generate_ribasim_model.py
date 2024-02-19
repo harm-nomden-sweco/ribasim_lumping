cd9b6bbf98d189afc6ca453e01d14ca65b262ee7
boyandomhof <44698491+boyandomhof@users.noreply.github.com>
2024-02-19 17:28:24 +0100

    added func get_direct_downstream_structures_for_nodes

--- ribasim_lumping/ribasim_model_generator/generate_ribasim_model.py
+++ ribasim_lumping/ribasim_model_generator/generate_ribasim_model.py
@@ -3,8 +3,13 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 
 import geopandas as gpd
 import pandas as pd
+import numpy as np
 import ribasim
 from shapely.geometry import LineString
+import networkx as nx
+
+from ..ribasim_network_generator.generate_ribasim_network import create_graph_based_on_nodes_edges
+from ..utils.general_functions import split_edges_by_split_nodes
 
 
 def generate_ribasim_nodes_static(
@@ -304,3 +309,99 @@ def generate_ribasim_model(
     ribasim_model.results_dir = results_dir
     return ribasim_model
 
+
+def get_direct_downstream_structures_for_nodes(
+        structures: gpd.GeoDataFrame,
+        edges: gpd.GeoDataFrame,    
+        nodes: gpd.GeoDataFrame,
+        split_nodes: gpd.GeoDataFrame = None,
+    ) -> gpd.GeoDataFrame:
+    """
+    Get structure directly downstream of nodes within the same basin based on shortest path. 
+    Use this function to get the node no and the shortest path length to that node for weir and/or pump structures.
+    
+    Basin code is retrieved from edges.
+    The edges will be split on the structure locations before determining the most direct downstream structure.
+    Supply split nodes optionally to update structure point geometries with snapped split nodes to avoid duplication
+
+    Parameters
+    ----------
+    structures (gpd.GeoDataFrame):               
+        GeoDataFrame containing all structure geometries. Should include 'code' column
+    edges (gpd.GeoDataFrame):
+        GeoDataFrame containing edges. Should include a 'basin' column with the associated basin code
+    nodes (gpd.GeoDataFrame):
+        GeoDataFrame containing nodes (that are compliant with edges). These will be returned as a result with info of downstream structure
+    split_nodes (gpd.GeoDataFrame):
+        (optional) snapped split nodes to edges/nodes. Should include 'split_node_id' column which contains code that is used in structures geodataframe
+    
+    Returns
+    -------
+    GeoDataFrames with nodes with 2 columns added containing direct downstream structure node no and the path length from node to that structure
+    """
+
+    structures, edges, nodes_orig = structures.copy(), edges.copy(), nodes.copy()
+    # update structure point locations with already snapped points of split nodes
+    if split_nodes is not None:
+        structures.geometry = [split_nodes.loc[split_nodes['split_node_id'] == c]['geometry'].values[0]
+                        if c in split_nodes['split_node_id'] 
+                        else g 
+                        for c, g in zip(structures['code'], structures.geometry)]
+    # split edges on structure locations. this will also regenerate the edges and nodes (but preserves basin in edges)
+    structures, edges, nodes = split_edges_by_split_nodes(structures, edges=edges)
+    structures.columns = [c.lower() for c in structures.columns]
+    # initialize new columns
+    nodes['downstream_structure_code'] = np.nan
+    nodes['downstream_structure_node_no'] = np.nan
+    nodes['downstream_structure_path_length'] = np.nan
+    # go over each basin individually to restrict the network size
+    basin_nrs = np.sort(edges['basin'].unique())
+    basin_nrs = basin_nrs[basin_nrs >= 1]
+    for basin_nr in basin_nrs:
+        # select nodes, edges and structures within basin
+        _edges = edges.loc[edges['basin'] == basin_nr].copy()
+        _nodes = nodes.loc[np.isin(nodes['node_no'], np.unique(np.concatenate([_edges['from_node'], _edges['to_node']])))].copy()
+        _structures = structures.loc[np.isin(structures['node_no'], _nodes['node_no'])].copy()
+
+        if _structures.empty:
+            continue  # skip if no structures are within basin
+
+        # create graph
+        _graph = create_graph_based_on_nodes_edges(_nodes, _edges, add_edge_length_as_weight=True)
+
+        # get shortest paths between nodes (keeping in mind direction of edges in graph) and the lengths of those paths
+        paths = dict(nx.all_pairs_dijkstra_path(_graph))
+        path_lengths = dict(nx.all_pairs_dijkstra_path_length(_graph))
+
+        # select for each node the direct downstream structure (the one with the shortest path to node)
+        structure_nodes = {k: v for k, v in zip(_structures['node_no'].values, _structures['code'].values)}
+        store = {}
+        for k, vps in paths.items():
+            length_to_structures = {vp: path_lengths[k][vp] for vp in vps.keys() if vp in structure_nodes.keys()}
+
+            # select the structure and store together with structure code and distance from node to that structure
+            if k in structure_nodes.keys():
+                store[k] = (k, structure_nodes[k], 0)  # node is a structure
+            elif len(length_to_structures.keys()) == 0:
+                store[k] = None  # there is no downstream structure within basin for node
+            else:
+                n = list(length_to_structures.keys())[np.argmin(length_to_structures.values())]
+                store[k] = (n, structure_nodes[n], length_to_structures[n])
+
+        # save in nodes geodataframe
+        for k, v in store.items():
+            if v is not None:
+                nodes.loc[nodes['node_no'] == k, 'downstream_structure_node_no'] = v[0]
+                nodes.loc[nodes['node_no'] == k, 'downstream_structure_code'] = v[1]
+                nodes.loc[nodes['node_no'] == k, 'downstream_structure_path_length'] = v[2]
+    
+    # update original nodes geodataframe with downstream structure info using spatial join with max 5 cm buffer
+    nodes_updated = nodes_orig.copy()
+    nodes_updated = nodes_updated.sjoin_nearest(
+        nodes[['downstream_structure_node_no', 'downstream_structure_code', 'downstream_structure_path_length', 'geometry']], 
+        how='left', 
+        max_distance=0.05
+    )
+
+    return nodes_updated
+    
